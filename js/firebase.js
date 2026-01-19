a1871a8b489bcbbc2f9770b7163973a71e1068bd
jgranizod <joaquingranizo06@gmail.com>
2026-01-19 15:23:40 -0500

    Set up Firebase and Firestore connection
    Initialize Firebase app and connect to Firestore.

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD92apK91B1M3Jin0p9Jw_68G8uxlsu_Cw",
+  authDomain: "catalogo-bonsais.firebaseapp.com",
+  projectId: "catalogo-bonsais",
+  storageBucket: "catalogo-bonsais.firebasestorage.app",
+  messagingSenderId: "733197067098",
+  appId: "1:733197067098:web:03babf7f4542468cf68963"
+
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Conectar con Firestore
+export const db = getFirestore(app);
+
+console.log("🔥 Firebase conectado correctamente");

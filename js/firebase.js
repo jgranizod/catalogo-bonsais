a904bd9a50e58b9c72551c917163b97f22fd10d3
jgranizod <joaquingranizo06@gmail.com>
2026-01-19 18:39:11 -0500

    Update Firebase configuration with placeholder values

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD92apK91B1M3Jin0p9Jw_68G8uxlsu_Cw",
-  authDomain: "catalogo-bonsais.firebaseapp.com",
-  projectId: "catalogo-bonsais",
-  storageBucket: "catalogo-bonsais.firebasestorage.app",
-  messagingSenderId: "733197067098",
-  appId: "1:733197067098:web:03babf7f4542468cf68963"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);

2ce648e6b5eb26cee75c3bfe99b500e1cd5e47dc
jgranizod <joaquingranizo06@gmail.com>
2026-01-19 20:36:51 -0500

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,7 @@
 // 🛒 SISTEMA DE CARRITO CON LOCALSTORAGE
 
 // Configuración
-const WHATSAPP_NUMERO = '593986500805'; // ⚠️ CAMBIA ESTE NÚMERO
+const WHATSAPP_NUMERO = '593985700805'; // ⚠️ CAMBIA ESTE NÚMERO
 
 // Obtener carrito del localStorage
 function obtenerCarrito() {
